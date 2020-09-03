5ad288d8017fbb453957a66125503c3d395cb84f
Shady Coder <shadysaleh01@gmail.com>
2020-09-02 23:47:39 -0700

    Tested the project

--- README.md
+++ README.md
@@ -3,28 +3,31 @@
 [![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
 
 ## Description
-### Tast
+### A command line application that dynamically generates a professional README.md from a user's input, so allow the creator to spend more time working on finishing the project and less time creating a README.
 ## Table of Contents
 - [Instructions](#instructions)
 - [Usage](#usage)
 - [License](#license)
 - [Contibute](#contibute)
-- [Test](#test)
+- [Tests](#tests)
 - [Questions](#questions)
 ## Installation
 ### To install necessary dependencies, run the following command: 
-```Tast
+```
+Clone the repasitory and run the npm install to create a node-madules.
 ```
 ## Usage
-### Tast
-## Contibute
-### Tast
+### Run node index.js then answer the questions.
+## Contributing
+### It depends on the project
 ## License
 ### This project is licensed under the MIT license.
 
 ## Tests
 ### To run tests, run the following command: 
- ```Tast
+ ``` 
+index.js
  ```
 ## Questions
 ### If you have any questions about the repo, contact me directly at my [Email](mailto:shadysaleh01@gmail.com). You can find more of my work at my [GitHub](https://github.com/shadysaleh01).
+
--- index.js
+++ index.js
@@ -6,62 +6,47 @@ const questions = [
       type: "input",
       message: "What is the title of the project?",
       name: "title",
-      default: "Readme.md"
    },
-   // {
-   //    type: "input",
-   //    message: "Enter your badge code",
-   //    name: "badge",
-   //    default: "Tast"
-   // },
    {
       type: "input",
       message: "Please write a description of your project?",
       name: "description",
-      default: "Tast"
    },
    {
       type: "input",
       message: "What command should be run to install dependencies?",
       name: "installation",
-      default: "Tast"
    },
    {
       type: "input",
       message: "Desctibe usage",
       name: "usage",
-      default: "Tast"
    },
    {
       type: "rawlist",
       message: "What kind of license should your project have?",
       name: "license",
       choices: ["MIT", "APACHE2.0", "GPL3.0", "BSD3Clause", "None"],
-      default: "Tast"
    },
    {
       type: "input",
       message: "Please provide any details about contributing guidelines:",
-      name: "contribute",
-      default: "Tast"
+      name: "contributing",
    },
    {
       type: "input",
       message: "What command should be run to run tests?",
       name: "test",
-      default: "Tast"
    },
    {
       type: "input",
       message: "Please enter your Email address",
       name: "email",
-      default: "Tast"
    },
    {
       type: "input",
       message: "Please enter your GitHub username",
       name: "github",
-      default: "Tast"
    }
 ]
 
@@ -80,11 +65,12 @@ inquirer.prompt(questions).then((res) => {
       }
       console.log("Successfully! Created License")
    })
-
-   // fs.appendFileSync("README.md", ('\n' + "### " + res.badge) + '\n', function (err) {
-   //    if (err) throw err
-   //    console.log("Successfully! Created a Badge")
-   // })
+   fs.appendFileSync("README.md", "![Alt Text](https://drive.google.com/file/d/1ufmDXTfcrsYziI7gU-a8jKqeVNMyOanw/view)" + '\n', function (err) {
+      if (err) {
+         return console.log(err)
+      }
+      console.log("Success")
+   })
 
    fs.appendFileSync("README.md", ("## Description" + '\n' + "### " + res.description) + '\n', function (err) {
       if (err) {
@@ -93,14 +79,14 @@ inquirer.prompt(questions).then((res) => {
       console.log("Successfully! Created a Description")
    })
 
-   fs.appendFileSync("README.md", ("## Table of Contents" + '\n' + '- ' + ["[Instructions](#instructions)", "[Usage](#usage)", "[License](#license)", "[Contibute](#contibute)", "[Test](#test)", "[Questions](#questions)"].join('\n' + '- ')) + '\n', function (err) {
+   fs.appendFileSync("README.md", ("## Table of Contents" + '\n' + '- ' + ["[Instructions](#instructions)", "[Usage](#usage)", "[License](#license)", "[Contibute](#contibute)", "[Tests](#tests)", "[Questions](#questions)"].join('\n' + '- ')) + '\n', function (err) {
       if (err) {
          return console.log(err)
       }
       console.log("Successfully! Created a Table")
    })
 
-   fs.appendFileSync("README.md", ("## Installation" + '\n' + "### To install necessary dependencies, run the following command: \n```" + res.installation) + "\n```" + '\n', function (err) {
+   fs.appendFileSync("README.md", ("## Installation" + '\n' + "### To install necessary dependencies, run the following command: \n```\n" + res.installation) + "\n```" + '\n', function (err) {
       if (err) {
          return console.log(err)
       }
@@ -113,11 +99,11 @@ inquirer.prompt(questions).then((res) => {
       console.log("Successfully! Created a Usage")
    })
 
-   fs.appendFileSync("README.md", ("## Contibute" + '\n' + "### " + res.contribute) + '\n', function (err) {
+   fs.appendFileSync("README.md", ("## Contributing" + '\n' + "### " + res.contributing) + '\n', function (err) {
       if (err) {
          return console.log(err)
       }
-      console.log("Seccessfully! Created Contribute")
+      console.log("Seccessfully! Created Contributing")
    })
    fs.appendFileSync("README.md", ("## License" + '\n' + "### This project is licensed under the " + [res.license] + " license." + '\n') + '\n', function (err) {
       if (err) {
@@ -125,17 +111,17 @@ inquirer.prompt(questions).then((res) => {
       }
       console.log("Successfully! Created License")
    })
-   fs.appendFileSync("README.md", ("## Tests" + '\n' + "### To run tests, run the following command: \n ```" + res.test) + "\n ```" + '\n', function (err) {
+   fs.appendFileSync("README.md", ("## Tests" + '\n' + "### To run tests, run the following command: \n ``` \n" + res.tests) + "\n ```" + '\n', function (err) {
       if (err) {
          return console.log(err)
       }
-      console.log("Successfully! Created Test")
+      console.log("Successfully! Created Tests")
    })
    fs.appendFileSync("README.md", ("## Questions" + '\n' + "### If you have any questions about the repo, contact me directly at my [Email](mailto:" + res.email + "). You can find more of my work at my [GitHub](https://github.com/" + res.github + ").") + '\n', function (err) {
       if (err) {
          return console.log(err)
       }
-      console.log("Successfully! Created Test")
+      console.log("Successfully! Created Contacts")
    })
    console.log("Generating README...")
 })
